c15a2c5e9d1991fdc7d31c63152be0d68a2da982
TiyaAnlite <chenquanyu1999@126.com>
2018-02-08 00:33:00 +0800

    [Pre]用户注册部分
    用户读写器完善
    当日工作结束节点

--- Controller.py
+++ Controller.py
@@ -14,15 +14,32 @@ class UserReader(object): #用户数据读写器
             self.UserRead = False
             print "[Controller] UserReader Callback: ", identifier #将抛出的错误输出到控制台
 
+    def register(self, user, key=False): #用户注册
+        file = open("config/apikey.json", "r+")
+        api_key = json.loads(file.read())
+        NickName = None
+        Permission = { "AccountBook": False, "DevZone": False}
+        if key:
+            try:
+                 if api_key[key][isUsed]:
+                     raise MyException("This Api Key has benn used: ",key)
+                 NickName = api_key[key][]
+            except expression as identifier:
+                print "[Controller] UserRegister Callback: ", identifier
+
+
+
 class CallBackReader(object): #返回码解析对象，服务初始化时调用
     def __init__(self):
         file = open("config/callback.json", "r")
         self.Data = json.loads(file.read())
+        file.close()
 
 class ListReader(object): #菜单列表解析对象，服务初始化时调用
     def __init__(self):
         file = open("config/list.json", "r")
         self.Data = json.loads(file.read())
+        file.close()
 
 class ContentReader(object): #文本解析中心
     pass
@@ -32,4 +49,6 @@ class ContentReader(object): #文本解析中心
 def input(User, Content, IOCallBack, IOList):
     User = UserReader(User)
     if User.UserRead: #用户鉴权
+        pass
+    else:
         pass
