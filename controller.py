c7b80193f7bcd750725da304025219c516090429
TiyaAnlite <chenquanyu1999@126.com>
2018-03-05 20:06:00 +0800

    debug

--- controller.py
+++ controller.py
@@ -160,7 +160,7 @@ class ContentReader(object): #文本解析中心
 
 def input(User, Content, IOList): #流水线，注意由于没有IOCallback，返回的必须是键值
     User = UserReader(User)
-    print "[COM]Created User object'
+    print "[COM]Created User object"
     if User.Read: #用户鉴权
         Reader = ContentReader(User.Data, Content, IOList) #先传入，初始化
         User.Data, callback = ContentReader.process() #再处理，接受输出
