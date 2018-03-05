e6b572f7f9856119f10ba26ac0860e59c98d6e04
TiyaAnlite <chenquanyu1999@126.com>
2018-03-05 20:14:55 +0800

    debug

--- controller.py
+++ controller.py
@@ -183,6 +183,6 @@ def input(User, Content, IOList): #流水线，注意由于没有IOCallback，�
 
         except: #输入的不是数字
             print "[COM]illegal"
-            return "Content.illegal"
+            return ["Content.illegal"]
 
 
