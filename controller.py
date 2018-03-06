604a0601b8103ceba8b07c5409ce6bb50024bf49
TiyaAnlite <chenquanyu1999@126.com>
2018-03-06 12:44:18 +0800

    debug

--- controller.py
+++ controller.py
@@ -178,6 +178,8 @@ def input(User, Content, IOList): #流水线，注意由于没有IOCallback，�
 
     else: #非法用户区域
         print "[Com]Unreg"
+        try:
+            Content = int(Content)
         if isinstance(Content,int): #未注册用户输入的是数字？
             print "[Com]key"
             if Content == 0: #无key注册模式
