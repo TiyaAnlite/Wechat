ad37d16be6784b9488405fff5bcf0c1c658c2586
TiyaAnlite <chenquanyu1999@126.com>
2018-04-05 08:53:30 +0800

    模块：AccountBook build input, checkout, transfer fuction

--- model/AccountBook.py
+++ model/AccountBook.py
@@ -1,4 +1,46 @@
 # -*- coding: utf-8 -*-
 # filename: AccountBook.py
 
-def input(model):
+import model.Mail as Mail
+import json
+import time
+
+def input(Call, Content, Userdata):
+    Account = AccountBook(Content, Userdata)
+    exp = "Account." + Call + "()"
+    eval(exp)
+    callback, data = Account.callback()
+    return callback, data
+    
+class AccountBook(object):
+    def __init__(self, Content, Data):
+        self.Content = Content
+        self.Data = Data
+        self.FuctionTag = False
+        self.UserAccount = str(self.Data["Data.AccountBook"]["Count"])
+        self.Transfer = str(self.Data["Data.AccountBook"]["TransferCount"])
+        self.UserName = self.Data["NickName"]
+        file = open("model/data/accountbook.json","r")
+        self.Appdata = json.loads(file.read())
+        file.close()
+        
+    def checkout(self):
+        self.FuctionTag = "checkout"
+        
+    def transfer(self):
+        UserAccount = int(self.UserAccount)
+        try:
+            Request = int(self.content) #预防用户输入值非法
+        else:
+            if UserAccount >= Request:
+                self.Transfer = str(int(self.Transfer) + Request)
+                self.UserAccount = str(UserAccount - Request)
+                self.FuctionTag = "transfer.successed"
+                y, m, d, h, mi, s, null, null, null = time.localtime(time.time())
+                order_id = "T0297" + str(y) + str(m) + str(d) + str(h) + str(mi) + str(s)
+                maildata = [order_id, time.asctime(time.localtime(time.time)), self.UserName, str(Request), self.Transfer, self.UserAccount]
+                Mail.input("dev", "AccountBook.transfer", maildata)
+            else:
+                self.FuctionTag = "transfer.error"
+        except:
+            pass
